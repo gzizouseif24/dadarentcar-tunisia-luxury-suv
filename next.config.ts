90cbdd2855a27069730325b8fedbc36e5cc9fefb
gzizou24 <gzizou.seif@gmail.com>
2025-11-09 21:33:17 +0100

    cc

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
+  // outputFileTracingRoot: path.resolve(__dirname, '../../'),
   typescript: {
     ignoreBuildErrors: true,
   },
